2d1ba8d7c949c74516e625259dfb5b83a4b30f8b
deepali <deepaalipatil@gmail.com>
2021-12-28 00:02:28 +0530

    ADDED: Perform bubble sort

---
+++ src/com/bridgelabz/algorithm/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.algorithm;
+
+public class BubbleSort {
+      
+
+	private static void printArray(int[] arr) {
+		for(int i=0; i<arr.length;i++) {
+			System.out.println(arr[i]+"");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		int arr[] = {5,7,3,2,8};
+		
+		for(int i=0; i< arr.length-1;i++) {
+			for(int j=0;j<arr.length-i-1;j++) {
+				if (arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1]=temp;
+				}
+				
+			}
+		}
+		
+       printArray(arr);
+	}
+
+
+}
